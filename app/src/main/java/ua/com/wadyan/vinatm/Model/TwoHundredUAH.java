5db1354692b8dc97d354af9c47176836c62f12f1
Wad + <wadyan.r@gmail.com>
2016-11-02 00:01:12 +0200

    display count of eject UAH

--- app/src/main/java/ua/com/wadyan/vinatm/Model/TwoHundredUAH.java
+++ app/src/main/java/ua/com/wadyan/vinatm/Model/TwoHundredUAH.java
@@ -5,5 +5,27 @@ package ua.com.wadyan.vinatm.Model;
  */
 
 public class TwoHundredUAH extends UAH{
-    final private int nominal = 200;
+    final static private int nominal = 200;
+    private static int reminder;
+    private static int toEject;
+
+    public static int getNominal() {
+        return nominal;
+    }
+
+    public static int getReminder() {
+        return reminder;
+    }
+
+    public static void setReminder(int reminder) {
+        TwoHundredUAH.reminder = reminder;
+    }
+
+    public static int getToEject() {
+        return toEject;
+    }
+
+    public static void setToEject(int toEject) {
+        TwoHundredUAH.toEject = toEject;
+    }
 }
